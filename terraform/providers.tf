12bdf8c3f8226a9cf9016c36be13c796dde8d10f
maximehamondpro-blip <maxime.hamond.pro@gmail.com>
2026-07-01 11:01:58 +0000

    feat: ajout du CI/CD complet avec AssumeRole et Terraform Apply

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,22 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
+
 provider "aws" {
   region = var.aws_region
-  default_tags { tags = { Project = "ynov-iac-2025" } }
 
   assume_role {
-    role_arn     = "arn:aws:iam::738563260931:role/role_etudiants"
-    session_name = "tp-final-mh"
+    role_arn = "arn:aws:iam::738563260931:role/role_etudiants"
+  }
+
+  default_tags {
+    tags = {
+      Project = "ynov-iac-2025"
+    }
   }
 }
